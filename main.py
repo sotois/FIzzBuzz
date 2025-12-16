ce9c5e138bcedc1d6a9a3003fe05ddfc8887384c
sotois <kofiagyapong837@gmail.com>
2025-12-16 12:12:46 -0500

    Implement FizzBuzz function and play logic

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-
+def check_fizzbuzz(number):
+  if number % 3 == 0 and number % 5 == 0:
+    return "Fizz-Buzz"
+  
+  elif number % 3 == 0:
+    return "Fizz"
+    
+  elif number % 5 == 0:
+      return "Buzz"
+    
+  else:
+    return str(number)
+    
+def play_fizzbuzz():
+  for i in range(1, 101):
+    print(check_fizzbuzz(i))
+    
+play_fizzbuzz()
